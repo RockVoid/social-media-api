5003ec3704436f2bbbd1311be3fc140b4fb2379c
Roque <devroque18@gmail.com>
2023-12-04 10:51:31 -0300

    [FEAT]: Post controller and routes

--- controllers/post.js
+++ controllers/post.js
@@ -1,7 +1,75 @@
-//
-//  SELECT * FROM posts AS p JOIN users AS u ON (u.id = p.userId)
-//  Modo simplificado:
-//  
-//  SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId);
-//
-//
+import { db } from "../connect.js";
+import jwt from "jsonwebtoken";
+import { tokenIsValid } from "../utils/tokenIsValid.js";
+import moment from "moment";
+
+export const addPost = (req, res) => {
+  const token = tokenIsValid(req.headers.cookies, res);
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const q = "INSERT INTO posts(desc,img,createdAt,userId) VALUES(?,?,?,?)";
+    const values = [
+      req.body.desc,
+      req.body.img,
+      moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+      userInfo.id
+    ];
+
+    db.query(q, [...values], (err) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("Post has been created!");
+    });
+  })
+};
+
+export const deletePost = (req, res) => {
+  const token = tokenIsValid(req.headers.cookies, res);
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json(err);
+
+    const getPostsFromUser = "SELECT * FROM posts WHERE idUser=?";
+    db.query(getPostsFromUser, [userInfo.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+      if (data.affectedRows > 0) return res.status(201).json("Post deleted!")
+      return res.status(403).json("You can delete only your post!")
+    });
+
+    const q = "DELETE FROM posts WHERE id=?";
+    db.query(q, [req.params.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+      if (data.affectedRows > 0) return res.status(201).json("Post deleted!")
+      return res.status(403).json("You can delete only your post!")
+    });
+  });
+}
+
+export const getPosts = (req, res) => {
+  //  SELECT * FROM posts AS p JOIN users AS u ON (u.id = p.userId)
+  //  Modo simplificado:
+  //  
+  //  SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId);
+  //
+  //
+  const token = tokenIsValid(req.headers.cookies, res);
+  const userId = req.query.userId;
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const getTimelinePosts = 'SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ORDER BY p.createdAt DESC';  
+    const getSpecificUserPost = `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)
+    LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId= ? OR p.userId =?
+    ORDER BY p.createdAt DESC`;
+
+    const q = (userId !== "undefined" ? getTimelinePosts : getSpecificUserPost);
+    const values = (userId !== "undefined" ? [userId] : [userInfo.id, userInfo.id]);
+
+    db.query(q, [...values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json(data);
+    });
+  });
+}
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import express from "express";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/user.js";
 import storieRoutes from "./routes/story.js";
+import commentRoutes from "./routes/comments.js";
+import relationshipRoutes from "./routes/relationship.js";
+import postRoutes from "./routes/post.js";
 
 import cookieParser from "cookie-parser";
 
@@ -13,6 +16,10 @@ app.use(express.json());
 app.use("/api", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/stories", storieRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/relationships", relationshipRoutes);
+app.use("/api/posts", postRoutes);
+
 app.use(cookieParser());
 
 app.listen(8000, () => {
